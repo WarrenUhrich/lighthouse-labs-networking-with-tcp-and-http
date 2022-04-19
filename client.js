895674615ca3b5095ff554380061c7618bdfbb12
Warren <hello@warren.codes>
2022-04-19 12:56:07 -0600

    Add terminal submission to client.js

--- client.js
+++ client.js
@@ -6,3 +6,10 @@ const config = {
 };
 
 const client = net.createConnection(config);
+
+client.setEncoding('utf-8');
+
+// Read terminal submission; send to server.
+process.stdin.on('data', (data) => {
+    client.write(data);
+});

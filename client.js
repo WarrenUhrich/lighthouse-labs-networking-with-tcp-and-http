fc92b2875231b71afde39051a5a1ca660d2be55e
Warren <hello@warren.codes>
2022-08-31 10:38:38 -0600

    Update server and client to run a chat room!

--- client.js
+++ client.js
@@ -1,13 +1,27 @@
 const net = require('net');
 
-// const config = {
-//     host: 'localhost', // 127.0.0.1
-//     port: 5050
-// };
-
 const config = {
-    host: '2.tcp.ngrok.io',
-    port: '18683'
+    host: 'localhost', // 127.0.0.1
+    port: 5050
 };
 
+// const config = {
+//     host: '2.tcp.ngrok.io',
+//     port: '18683'
+// };
+
 const client = net.createConnection(config);
+
+client.setEncoding('utf-8');
+
+client.on('data', (data) => {
+    console.log(data);
+});
+
+client.write('Hello, I am the client!');
+
+// Standard input from Node.js
+// When you type into terminal and hit enter.
+process.stdin.on('data', (userMessage) => {
+    client.write(userMessage);
+});
--- server.js
+++ server.js
@@ -8,6 +8,33 @@ server.listen(PORT, () => {
     console.log('The server is now listening on port:', PORT);
 });
 
-server.on('connection', () => {
+const allConnections = [];
+
+server.on('connection', (connection) => {
     console.log('A client has connected to the server.');
+
+    connection.setEncoding('utf-8');
+
+    connection.write('You have successfully connected to our Lighthouse TCP test server!');
+
+    connection.on('data', (data) => {
+        console.log(data.trim());
+
+        if (data.trim().startsWith('name: ')) {
+            const name = data.trim().split(' ')[1];
+            return connection.username = name;
+        }
+
+        for (const conn of allConnections) {
+            if (conn !== connection && !conn._writableState.finished) {
+                if (connection.username) {
+                    conn.write(`${connection.username.trim()}: ${data.trim()}`);
+                } else {
+                    conn.write(`Unknown: ${data.trim()}`);
+                }
+            }
+        }
+    });
+
+    allConnections.push(connection);
 });

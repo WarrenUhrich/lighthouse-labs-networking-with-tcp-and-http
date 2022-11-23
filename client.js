1b347e3e5bd275a33eb9caba3b9a8e0b83848458
Warren <hello@warren.codes>
2022-11-23 10:56:55 -0700

    Add chat / username feature(s)

--- client.js
+++ client.js
@@ -3,6 +3,8 @@ const net = require('net');
 const config = {
     host: 'localhost', // '127.0.0.1'
     port: 5000
+    // host: '6.tcp.ngrok.io',
+    // port: 11872
 };
 
 const client = net.createConnection(config);
@@ -10,7 +12,7 @@ const client = net.createConnection(config);
 client.setEncoding('utf-8');
 
 client.on('data', (data) => {
-    console.log('Data from Server:', data);
+    console.log(data);
 });
 
 process.stdin.on('data', (userInput) => {
--- server.js
+++ server.js
@@ -7,14 +7,36 @@ server.listen(PORT, () => {
     console.log('TCP Server is listening on PORT:', PORT);
 });
 
+const allConnections = [];
+
 server.on('connection', (connection) => {
     console.log('Client has connected!');
+    // console.log(connection);
 
     connection.setEncoding('utf-8');
 
     connection.write('Welcome! You have established a connection to the TCP server!');
 
     connection.on('data', (userInput) => {
+        userInput = userInput.trim();
+
+        if (userInput.startsWith('name:')) {
+            // name:warren
+            // ['name', 'warren']
+            const name = userInput.split(':')[1];
+            connection.username = name;
+            return;
+        }
+
         console.log('Data from Client:', userInput);
+
+        for (const connFromArr of allConnections) {
+            if (connFromArr !== connection) {
+                const name = connection.username ? connection.username : 'Guest';
+                connFromArr.write(`${name}: ${userInput}`);
+            }
+        }
     });
+
+    allConnections.push(connection);
 });
